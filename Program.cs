2197e46be449d123dcbf4a82f7508e079c03b0f7
Vikas Vishwakarma <Vikasvk17@outlook.com>
2024-11-22 16:56:43 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);  // Creates a WebApplication builder
 var app = builder.Build();  // Builds the app
 
+// Enable serving static files from wwwroot
+app.UseStaticFiles();  // This will allow serving static files
+
 // Map a simple GET route that returns "Hello, World!"
 app.MapGet("/", () => "Hello, World!");
 
